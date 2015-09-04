6024ae3c14ca882b6eb5df5f0b4dbf8ad4d3875d
Ryan Flynn <r.d.flynn@gre.ac.uk>
2015-09-04 16:15:15 +0100

    First commit
    Blah

---
+++ CPDACardboard/Assets/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+	public GameObject objectToRotate;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		objectToRotate.transform.Rotate(Vector3.right * (Time.deltaTime*10.0f));
+		objectToRotate.transform.Rotate(Vector3.up * Time.deltaTime, Space.World);
+	}
+}
